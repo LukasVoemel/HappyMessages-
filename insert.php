b9daa8a00b65559ce2639cfe2decb432b38327ca
Gabriella Gage <gaga9562@colorado.edu>
2023-02-17 23:34:44 -0700

    php 2

--- insert.php
+++ insert.php
@@ -1,22 +1,36 @@
 <?php
-$con = mysqli_connect('localhost','root','');
+ 
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        $conn = mysqli_connect("localhost", "root", "", "user_info");
+         
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. "
+                . mysqli_connect_error());
+        }
+         
+        // Taking all 5 values from the form data(input)
+        $tel =  $_REQUEST['tel'];
 
-if(!$con) {
-    echo 'Not connected to server!';
-}
-
-if(!mysqli_select_db($con,'user_info')) {
-    echo 'Database not selected!';
-}
-
-$tel = $_POST['tel'];
-
-
-$sql = "INSERT INTO phone_numbers (Name,Email) VALUES ('$Name','$Email')";
-if(!mysqli_query($con,$sql)) {
-    echo 'Data not inserted';
-} else {
-    echo 'Data inserted';
-}
-//header("refresh:2; url=form.html");
-?>
+         
+        // Performing insert query execution
+        // here our table name is college
+        $sql = "INSERT INTO phone_numbers  VALUE ('$tel')";
+         
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully."
+                . " Please browse your localhost php my admin"
+                . " to view the updated data</h3>";
+ 
+            echo nl2br("\n$tel);
+        } else{
+            echo "ERROR: Hush! Sorry $sql. "
+                . mysqli_error($conn);
+        }
+         
+        // Close connection
+        mysqli_close($conn);
+        ?>
